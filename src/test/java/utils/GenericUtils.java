43a5db84795965d1faab5e963004cff53533622e
kunalchavan <kunal24chavan@gmail.com>
2025-07-27 13:39:39 +0530

    Parallel Execution

--- src/test/java/utils/GenericUtils.java
+++ src/test/java/utils/GenericUtils.java
@@ -49,4 +49,13 @@ public class GenericUtils extends TestBase {
         }
     }
 
+    public void SwitchWindowToChild()
+    {
+        Set<String> s1=driver.getWindowHandles();
+        Iterator<String> i1 =s1.iterator();
+        String parentWindow = i1.next();
+        String childWindow = i1.next();
+        driver.switchTo().window(childWindow);
+    }
+
 }
